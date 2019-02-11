ac4275a315031b6feacb60387d4edfde23cf08ca
Sticky Chen <sticky.chen@mediatek.com>
2019-02-11 14:31:22 +0800

    Prevent scan searching overhead
    If user connects to a AP but supplicant fails to
    connect it successfully, supplicant will trigger
    periodical scan to search the AP. The searching
    scan brings redudant power consumption under
    screen off.
    To prevent the overhead, we remove networks to
    supplicant before starting PNO scan.
    
    Test: Connect to a nonexistent AP, screen off and
    make sure there is no searching scan triggered by
    supplicant.
    Bug: 124281812
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I51c8c679e1d1cf268c4f3235f348d07224aac93d

--- service/java/com/android/server/wifi/scanner/WificondScannerImpl.java
+++ service/java/com/android/server/wifi/scanner/WificondScannerImpl.java
@@ -415,6 +415,7 @@ public class WificondScannerImpl extends WifiScannerImpl implements Handler.Call
     }
 
     private boolean startHwPnoScan(WifiNative.PnoSettings pnoSettings) {
+        mWifiNative.removeAllNetworks(mIfaceName);
         return mWifiNative.startPnoScan(mIfaceName, pnoSettings);
     }
 
